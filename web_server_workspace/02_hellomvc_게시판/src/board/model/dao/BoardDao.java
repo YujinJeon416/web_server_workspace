45523d88056b47aaca51b140a3c27d8b09d14584
Yujin Jeon <devjeon416@gmail.com>
2021-04-05 21:58:43 +0900

    Add files via upload

---
+++ web_server_workspace/02_hellomvc_게시판/src/board/model/dao/BoardDao.java
@@ -0,0 +1,124 @@
+package board.model.dao;
+
+import static common.JDBCTemplate.close;
+
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import board.model.vo.Board;
+
+public class BoardDao {
+	
+	private Properties prop = new Properties();
+	
+	public BoardDao() {
+		//board-query.properties의 내용읽어와서 prop에 저장
+		//resources/sql/board-query.properties가 아니라
+		//WEB-INF/classes/sql/board/board-query.properties에 접근해야함.
+		String fileName = "/sql/board/board-query.properties";
+		String absPath = BoardDao.class.getResource(fileName).getPath();
+		try {
+			prop.load(new FileReader(absPath));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public List<Board> selectList(Connection conn, int start, int end) {
+		List<Board> list = new ArrayList<>();
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String sql = prop.getProperty("selectList");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, start);
+			pstmt.setInt(2, end);
+			rset = pstmt.executeQuery();
+			while(rset.next()) {
+				Board board = new Board();
+				board.setNo(rset.getInt("no"));
+				board.setTitle(rset.getString("title"));
+				board.setWriter(rset.getString("writer"));
+				board.setContent(rset.getString("content"));
+				board.setRegDate(rset.getDate("reg_date"));
+				board.setReadCount(rset.getInt("read_count"));
+				list.add(board);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		return list;
+	}
+
+	public int selectBoardCount(Connection conn) {
+		int totalContents = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String sql = prop.getProperty("selectBoardCount");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rset = pstmt.executeQuery();
+			while(rset.next()) {
+				totalContents = rset.getInt("cnt");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		return totalContents;
+	}
+	
+	public int insertBoard(Connection conn, Board board) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql = prop.getProperty("insertBoard"); 
+		System.out.println(sql);
+		
+		try {
+			//미완성쿼리문을 가지고 객체생성.
+			pstmt = conn.prepareStatement(sql);
+			//쿼리문미완성
+			pstmt.setString(1, board.getTitle());
+			pstmt.setString(2, board.getWriter());
+			pstmt.setString(3, board.getContent());
+			
+			
+			//쿼리문실행 : 완성된 쿼리를 가지고 있는 pstmt실행(파라미터 없음)
+			//DML은 executeUpdate()
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		
+		return result;
+	}
+
+	
+	
+
+}
+
+
+
+
+
+
+
+
